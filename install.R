4be1da00c8b61634a8b6cb9f8bcf98c0c7f3d9f9
Matt Ingram <40709229+mattcingram@users.noreply.github.com>
2023-05-25 10:41:59 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,3 +1,4 @@
 # packages for analysis
-mypackages <- c("sf", "spdep", "spatialreg", "rgdal", "terra", "ggplot2", "ggmap", "gridExtra", "foreign", "haven", "xtable", "stargazer", "tictoc", "plyr", "dplyr", "CARBayes", "miceadds", "tidyverse", "sandwich", "GWmodel")
+mypackages <- c("sf", "spdep", "spatialreg", "rgdal", "terra", "ggmap", "GWmodel",
+"ggplot2", "gridExtra", "foreign", "haven", "xtable", "stargazer", "tictoc", "plyr", "dplyr", "CARBayes", "miceadds", "tidyverse", "sandwich", "pacman")
 install.packages(mypackages, verbose=TRUE)
