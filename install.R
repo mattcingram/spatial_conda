f70cf24498793960565cfbdb4926ff856a7029b0
Matt Ingram <40709229+mattcingram@users.noreply.github.com>
2023-05-30 13:15:20 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,1 @@
-# packages for analysis
-#mypackages <- c("sf", "spdep", "spatialreg", "rgdal", "terra", "ggmap", "GWmodel", "ggplot2", "gridExtra", "foreign", "haven", "xtable", "stargazer", "tictoc", "plyr", "dplyr", "CARBayes", "miceadds", "tidyverse", "sandwich", "pacman")
-#install.packages(mypackages, verbose=TRUE)
-# additional packages
 install.packages(c("pacman"))
